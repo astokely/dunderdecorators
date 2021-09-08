3c318ee6475925901e647039db7ba15e8f4f4783
astokely1994@pointloma.edu <astokely1994@pointloma.edu>
2021-09-08 12:07:44 -0600

    Added documentation and doc-string examples

--- dunderdecorators/dunder_decorators.py
+++ dunderdecorators/dunder_decorators.py
@@ -16,6 +16,83 @@ def dunder_iter(
 		attr: Optional[str] = None,
 		slots: Optional[bool] = None,
 ) -> Cls:
+	"""
+	Adds an __iter__ special method to the decorated class.
+
+	Parameters
+	---------
+	cls : User Defined Class
+		Class that is decorated
+
+	attr : str, optional
+		Name of class object attribute that __iter__
+		is defined with repect to. If None, dunder_iter 
+		defines __iter__ with respect to the class object's
+		__dict__ or __slots__. Defaults to None.
+
+	slots : bool, optional
+		If True, dunder_iter defines __iter__ with respect to
+		the class object's __slots__ attribute. Else, __iter__
+		is defined with respect to the class object's dictionary
+		or attribute specified by attr. Defaults to None.
+
+	Returns
+	-------
+	: User Defined Class
+
+	Examples
+	--------
+	Add an __iter__ method to a class and have 
+	it defined with respect to the class object's
+	dictionary.
+
+	>>> @dunder_iter
+	>>> Class A(object):
+	>>>		pass
+	>>> a = A()
+	>>> a.a = 1
+	>>> a.b = 2
+	>>> a.c = 3
+	>>> for i in a:
+	>>> 	print(i)
+		1
+		2	
+		3
+	
+	Add an __iter__ method to a class and have 
+	it defined with respect to one of the class
+	objects iterable attributes.
+
+	>>> @dunder_iter(attr='a')
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A([1, 2, 3])
+	>>> for i in a:
+	>>> 	print(i)
+		1
+		2	
+		3
+
+	Add an __iter__ method to a class and have it
+	defined with repsect to the class's __slots__
+	attribute. 
+
+	>>> @dunder_iter(slots=True)
+	>>> Class A(object):
+	>>>		__slots__=('a', 'b', 'c')
+	>>>		def __init__(self, a, b, c):
+	>>>			self.a = a	
+	>>>			self.b = b	
+	>>>			self.c = c	
+	>>> a = A(1, 2, 3)
+	>>> for i in a:
+	>>> 	print(i)
+		1
+		2	
+		3
+
+	"""
 	def wrap(
 			cls: Cls,
 	) -> Cls:
@@ -72,6 +149,91 @@ def dunder_setitem(
 		attr: Optional[str] = False, 
 		slots: Optional[bool] = None,
 ) -> Cls:
+	"""
+	Adds a __setitem__ special method to the decorated class.
+
+	Parameters
+	---------
+	cls : User Defined Class
+		Class that is decorated
+
+	attr : str, optional
+		Name of class object attribute that __setitem__
+		is defined with repect to. The attribute is required
+		to either be a mapping or an iterable. If the attribute is
+		a mapping, the key used is one of the mapping's hash keys.
+		If the attribute is an iterable, the key is an index. If the 
+		index is out of bounds, the value is automatically appended to
+		the left or right end of the iterable, depending on the value of
+		the key. If None, dunder_setitem defines __setitem__ with 
+		respect to the class object's __dict__ or __slots__. 
+		Defaults to None.
+
+	slots : bool, optional
+		If True, dunder_setitem defines __setitem__ with respect to
+		the class object's __slots__ attribute. Else, __setitem__
+		is defined with respect to the class object's dictionary
+		or attribute specified by attr. Defaults to None.
+
+	Returns
+	-------
+	: User Defined Class
+
+	Examples
+	--------
+	Add a __setitem__ method to a class and have 
+	it defined with respect to the class object's
+	dictionary.
+
+	>>> @dunder_setitem
+	>>> Class A(object):
+	>>>		pass
+	>>> a = A()
+	>>> a['a'] = 1
+	>>> print(a.a)
+		1
+	
+	Add a __setitem__ method to a class and have 
+	it defined with respect to one of the class
+	objects iterable attributes.
+	
+	>>> @dunder_setitem(attr='a')
+		#where the attribute is a non-mapping iterable
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A([1, 2, 3])
+	>>> a[1] = 1
+	>>> print(a.a)
+		[1, 1, 3]
+	>>> 
+	>>> @dunder_setitem(attr='a')
+		#where the attribute is a mapping
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A({'a' : 1, 'b' : 2, 'c' : 3})
+	>>> a['a'] = 0
+	>>> print(a.a)
+		{'a' : 0, 'b' : 2, 'c' : 3}
+
+	Add a __setitem__ method to a class and have it
+	defined with repsect to the class's __slots__
+	attribute. 
+
+	>>> @dunder_setitem(slots=True)
+	>>> Class A(object):
+	>>>		__slots__=('a', 'b', 'c')
+	>>>		def __init__(self, a, b, c):
+	>>>			self.a = a	
+	>>>			self.b = b	
+	>>>			self.c = c	
+	>>> a = A(1, 2, 3)
+	>>> a['a'] = 5
+	>>> print(a.a)
+		5
+
+	"""
 	def wrap(
 			cls: Cls,
 	) -> Cls:
@@ -210,6 +372,86 @@ def dunder_getitem(
 		attr: Optional[str] = False,
 		slots: Optional[bool] = None,
 ) -> Cls:
+	"""
+	Adds a __getitem__ special method to the decorated class.
+
+	Parameters
+	---------
+	cls : User Defined Class
+		Class that is decorated
+
+	attr : str, optional
+		Name of class object attribute that __getitem__
+		is defined with repect to. The attribute is required
+		to either be a mapping or an iterable. If the attribute is
+		a mapping, the key used is one of the mapping's hash keys.
+		If the attribute is an iterable, the key is an index. If the 
+		If None, dunder_getitem defines __getitem__ with 
+		respect to the class object's __dict__ or __slots__. 
+		Defaults to None.
+
+	slots : bool, optional
+		If True, dunder_getitem defines __getitem__ with respect to
+		the class object's __slots__ attribute. Else, __getitem__
+		is defined with respect to the class object's dictionary
+		or attribute specified by attr. Defaults to None.
+
+	Returns
+	-------
+	: User Defined Class
+
+	Examples
+	--------
+	Add a __getitem__ method to a class and have 
+	it defined with respect to the class object's
+	dictionary.
+
+	>>> @dunder_getitem
+	>>> Class A(object):
+	>>>		pass
+	>>> a = A()
+	>>> a.a = 1
+	>>> print(a['a'])
+		1
+	
+	Add a __getitem__ method to a class and have 
+	it defined with respect to one of the class
+	objects iterable attributes.
+	
+	>>> @dunder_getitem(attr='a')
+		#where the attribute is a non-mapping iterable
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A([1, 2, 3])
+	>>> print(a[2])
+		3
+	>>> 
+	>>> @dunder_getitem(attr='a')
+		#where the attribute is a mapping
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A({'a' : 1, 'b' : 2, 'c' : 3})
+	>>> print(a['a'])
+		1
+
+	Add a __getitem__ method to a class and have it
+	defined with repsect to the class's __slots__
+	attribute. 
+
+	>>> @dunder_setitem(slots=True)
+	>>> Class A(object):
+	>>>		__slots__=('a', 'b', 'c')
+	>>>		def __init__(self, a, b, c):
+	>>>			self.a = a	
+	>>>			self.b = b	
+	>>>			self.c = c	
+	>>> a = A(1, 2, 3)
+	>>> print(a['a'])
+		1
+
+	"""
 	def wrap(
 			cls: Cls,
 	) -> Cls:
@@ -409,8 +651,90 @@ def dunder_missing(
 		cls: Optional[Cls] = None, 
 		attr: Optional[str] = False,
 		default_value: Optional[Any] = None,	
-		slots: Optional[bool] = None
 ) -> Any:
+	"""
+	Adds a __missing__ special method to the decorated class.
+	Must be used alongside the dunder_getitem decorator and
+	declared first (below the dunder_getitem decorator).	
+
+	Parameters
+	---------
+	cls : User Defined Class
+		Class that is decorated
+
+	attr : str, optional
+		Name of class object attribute that __missing__
+		is defined with repect to. The attribute is required
+		to be a mapping. If None, dunder_getitem defines 
+		__missing__ with respect to the class object's 
+		dictionary. Defaults to None.
+
+	default_value : Any, optional
+		Value that is mapped to the "non-existent" key __missing__
+		adds to the class object's dictionary or mapping attribute
+		specified by attr. Defaults to None. 
+
+
+	Returns
+	-------
+	: Any 
+
+	Examples
+	--------
+	Add a __missing__ method to a class and have 
+	it defined with respect to the class object's
+	dictionary.
+
+	>>> @dunder_getitem
+	>>> @dunder_missing
+	>>> Class A(object):
+	>>>		pass
+	>>> a = A()
+	>>> a['a']
+	>>> print(a['a'])
+		None
+
+	Add a __missing__ method to a class and have 
+	it defined with respect to the class object's
+	dictionary with a default value of 6.559.
+
+	>>> @dunder_getitem
+	>>> @dunder_missing
+	>>> Class A(object):
+	>>>		pass
+	>>> a = A()
+	>>> a['a']
+	>>> print(a['a'])
+		6.559
+	
+	Add a __missing__ method to a class and have 
+	it defined with respect to one of the class
+	objects mapping attributes.
+	
+	>>> @dunder_missing(attr='a')
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A({'a' : 1})
+	>>> a['b']
+	>>> print(a.a)
+		{'a' : 1, 'b' : None}
+
+	Add a __missing__ method to a class and have 
+	it defined with respect to one of the class
+	objects mapping attributes with a default
+	value of [1, 2, 3, 4, 5].
+	
+	>>> @dunder_missing(attr='a', default_value=[1, 2, 3, 4, 5])
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A({'a' : 1})
+	>>> a['b']
+	>>> print(a.a)
+		{'a' : 1, 'b' : [1, 2, 3, 4, 5]}
+
+	"""
 	def wrap(
 			cls: Cls,
 	) -> Cls:
@@ -481,7 +805,67 @@ def dunder_missing(
 def dunder_repr(
 		cls: Optional[Cls] = None, 
 		slots: Optional[bool] = None,
-) -> Any:
+) -> str:
+	"""
+	Adds a __repr__ special method to the decorated class.
+
+	Parameters
+	---------
+	cls : User Defined Class
+		Class that is decorated
+
+	slots : bool, optional
+		If True, dunder_repr defines __repr__ with respect to
+		the class object's __slots__ attribute. Else, __repr__
+		is defined with respect to the class object's dictionary.
+		Defaults to None.
+
+	Returns
+	-------
+	: str 
+
+	Examples
+	--------
+	Add a __repr__ method to a class and have 
+	it defined with respect to the class object's
+	dictionary.
+
+	>>> @dunder_repr
+	>>> Class A(object):
+	>>>		def __init__(self, a):
+	>>>			self.a = a	
+	>>> a = A(1)
+	>>> print(a)
+		A(a=1)
+	>>> a.b = 5.662
+	>>> print(a)
+		A(a=1, b=5.662)
+	>>> a.c = [1, 2, 3, 4, 5] 
+	>>> print(a)
+		A(a=1, b=5.662, c=[1, 2, 3, 4, 5])
+
+	Add a __missing__ method to a class and have 
+	it defined with respect to the class object's
+	__slots__ attribute
+
+	>>> @dunder_repr(slots=True)
+	>>> Class A(object):
+	>>>		__slots__=('a', 'b', 'c')
+	>>>		def __init__(self, a, b, c):
+	>>>			self.a = a	
+	>>>			self.b = b	
+	>>>			self.c = c	
+	>>> a = A(1, 5.662, [1, 2, 3, 4, 5])
+	>>> print(a)
+		A(a=1, b=5.662, c=[1, 2, 3, 4, 5])
+	>>> a.b = 6.332
+	>>> print(a)
+		A(a=1, b=6.332, c=[1, 2, 3, 4, 5])
+	>>> a.c = 6
+	>>> print(a)
+		A(a=1, b=6.332, c=6)
+
+	"""
 	def wrap(
 			cls: Cls,
 	) -> Cls:
