6ced693c329d631e61d66c9ae01ba9c39e1c1c28
astokely1994@pointloma.edu <astokely1994@pointloma.edu>
2021-09-08 12:30:04 -0600

    Added README

--- setup.py
+++ setup.py
@@ -2,8 +2,21 @@ import sys
 import os
 from setuptools import setup, find_packages, Extension
 
+description = (
+	'Python decorator library that '
+	+ 'adds customizeable dunder methods to '
+	+ 'decorated classes.'
+)
+
+with open('README.md', 'r') as f:
+	long_description = f.read()
+print(f)
+
 setup(
     name='dunderdecorators',
+	description=description,
+	long_description=long_description,
+	url='https://github.com/astokely/dunderdecorators',
     author='Andy Stokely',
     author_email='amstokely@ucsd.edu',
     license='MIT',
